02d9465ba6d9212d21f121cb13865acaf714054b
Chris Birster <christopher.birster@gmail.com>
2026-08-20 14:51:30 -0400

    Assemble homepage reference image data

---
+++ src/assets/homepageReference.ts
@@ -0,0 +1,7 @@
+import webp0 from "./mockup/webp0";
+import webp1 from "./mockup/webp1";
+import webp2 from "./mockup/webp2";
+import webp3 from "./mockup/webp3";
+import webp4 from "./mockup/webp4";
+
+export default `data:image/webp;base64,${webp0}${webp1}${webp2}${webp3}${webp4}`;
